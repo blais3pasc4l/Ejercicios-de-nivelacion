b66b977c145ab52bd204a93dd5a820300cd72edd
blais3pasc4l <juandavidcalderonpena@gmail.com>
2021-12-15 07:50:25 -0500

    first commit

---
+++ src/javascript/index.js
@@ -0,0 +1,28 @@
+var numero = 14 //numerica
+var letra = "juan" //string
+var booleans = true //booleans
+var arreglo = ["manzana", 12, true]
+var suma1 = 17 + 1;
+/* Una funcion es un bloque de codigo
+Que se usa para almacenar un una cadena indicaciones e invocarla y puede 
+o no retornar un valor */
+
+function suma (a, b) {
+    return a + b;
+}
+
+function  resta (a, b) {
+    return a - b;
+}
+
+function multiplicacion (a, b) {
+    return a * b;
+}
+
+//De tarea la funcion division
+//Y la funcion promedio (de dos numeros)
+// La suma de dos numeros divididos entre 2
+
+console.log('La suma es ' + suma(1,3));
+console.log('La resta es ' + resta(10,1));
+console.log('La multiplicacion ' + multiplicacion(5,14))
